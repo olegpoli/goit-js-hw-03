baa856fa10c9d44e107f7c75d2a69d31fdbe2d81
HP <zontiksm@ukr.net>
2024-07-19 21:02:33 +0300

    add index file

---
+++ js/task-2.js
@@ -0,0 +1,23 @@
+'use strict';
+
+function makeArray(firstArray, secondArray, maxLength) {
+
+let generalArray = firstArray.concat(secondArray);
+  
+
+  if (maxLength >= generalArray.length) { return generalArray }
+
+  else if (maxLength < generalArray.length) {
+    return generalArray.slice(0, maxLength) };
+
+
+
+};
+ 
+
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
